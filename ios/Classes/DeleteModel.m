60a0bc66cf3aa2f6e8f63920b069706160a6023d
mi6ock <miy606@gmail.com>
2020-03-24 11:50:08 +0900

    upstream firebase ml kit breaking change

--- ios/Classes/DeleteModel.m
+++ ios/Classes/DeleteModel.m
@@ -7,15 +7,14 @@
 + (void)handleEvent:(NSString *)text result:(FlutterResult)result {
   FIRTranslateLanguage modelName = FIRTranslateLanguageForLanguageCode(text);
 
-  NSSet<FIRTranslateRemoteModel *> *localModels =
-      [[FIRModelManager modelManager] availableTranslateModelsWithApp:FIRApp.defaultApp];
+  NSSet<FIRTranslateRemoteModel *> *localModels = [[FIRModelManager modelManager] downloadedTranslateModels];
 
   Boolean isModelPresent = [localModels
       containsObject:[FIRTranslateRemoteModel translateRemoteModelWithLanguage:modelName]];
 
   if (isModelPresent) {
     [[FIRModelManager modelManager]
-        deleteDownloadedTranslateModel:[FIRTranslateRemoteModel
+        deleteDownloadedModel:[FIRTranslateRemoteModel
                                            translateRemoteModelWithLanguage:modelName]
                             completion:^(NSError *_Nullable error) {
                               if (error != nil) {
